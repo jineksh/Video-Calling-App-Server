9e3af41db98c293584ee7ec77cdc382c3e210287
jineksh <chovatiyajinex@gmail.com>
2025-09-14 11:59:41 +0530

    add rooms memeber storage

--- src/handler/roomHandler.js
+++ src/handler/roomHandler.js
@@ -1,14 +1,26 @@
 import {v4 as uuid} from "uuid";
+
+const rooms = {};
 const roomHandler = (io,socket)=>{
+
+    
+
     const createRoom = ()=>{
         const roomId = uuid();
-        socket.join(roomId);
+        //socket.join(roomId);
         socket.emit("room-Created",{roomId});
         console.log("Room created with ID:", roomId);
+        rooms[roomId] = [];
     }
 
-    const joinRoom = ()=>{
-        console.log("Joining room");
+    const joinRoom = ({roomID,peerID})=>{
+        if(rooms[roomID]){
+            rooms[roomID].push(peerID);
+            console.log(`${peerID} is joining room: ${roomID}`);
+            socket.join(roomID);
+            console.log(rooms);
+        }
+        
     }
 
     socket.on("create-Room",createRoom);
